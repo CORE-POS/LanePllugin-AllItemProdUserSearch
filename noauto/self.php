ac864c5537915c0e99e1ca633a198ea4656df3ff
Andy Theuninck <andy@gohanman.com>
2016-06-06 13:36:22 -0500

    Init plugin

---
+++ noauto/self.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!class_exists('AllItemProdUserSearch', false)) {
+    include(__DIR__ . '/../AllItemProdUserSearch.php');
+}
+
+if (!class_exists('AllItemProductUserSearch', false)) {
+    include(__DIR__ . '/../AllItemProductUserSearch.php');
+}
+
